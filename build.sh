46b4b0ce2aa088afbc4913ee831b3c68b08642aa
hx-w <herixth@163.com>
2022-05-30 10:17:03 +0800

    fix script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ if [ "$(uname)" = "Darwin" ];then
     # clang++ -std=c++17 -Xpreprocessor -fopenmp src/*.cpp -o main
     clang++ -std=c++17 -fopenmp -lomp -I"$(brew --prefix libomp)/include" -L"$(brew --prefix libomp)/lib" src/*.cpp -o main
 elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ];then   
-    c++ -fopenmp -std=c++17 ./src/*.cpp -o main.exe
+    c++ -fopenmp -std=c++17 ./src/*.cpp -o main
 elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ];then    
     c++ -fopenmp -std=c++17 .\\src\\*.cpp -o main.exe
 fi
